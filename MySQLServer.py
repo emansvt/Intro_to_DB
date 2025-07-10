f7f5484162d13984ac7a59a8b14fd768eaa08d44
emansvt <emanmahmoudsvt@gmail.com>
2025-07-10 03:01:45 +0300

    Add files via upload

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Eman_mahmoud_svt93",
+
+)
+cursor = db.cursor()
+with open("alx_book_store.sql", "r", encoding="utf-8") as f:
+    sql=f.read()
+    
+for statement in sql.split(";"):
+    if statement.strip():
+        cursor.execute(statement)
+        
+print("Datebase 'alx_book_store' created successfully!")
+cursor.close()
+db.close()
+    
